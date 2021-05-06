d91f6268e36b77f295575de33e9e2ae7e961a822
nao <nownewnao777@gmail.com>
2021-05-07 07:55:06 +0900

    Added toast

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -5,16 +5,35 @@ import ProductCard from "../../components/Product/ProductCard";
 import { client } from "../../shopify/client";
 import Button from "@material-ui/core/Button";
 import { useCart } from "../../hooks/cart/use-cart"
+import React, { useState } from "react";
+import Snackbar, { SnackbarOrigin } from "@material-ui/core/Snackbar";
+import MuiAlert from '@material-ui/lab/Alert';
 
 type Props = {
   product: Product;
 };
 
+type ToastState = {
+  open: boolean;
+} & SnackbarOrigin;
+
+
+
 const ProductDetail = ({ product }: Props) => {
+  const [toastState, setToastState] = useState<ToastState>({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open } = toastState;
+  const close = () => {
+    setToastState({...toastState, open: false});
+  };
 
 const { addToCart }= useCart();
   const updateCart = async (id: string) => {
     await addToCart(id);
+    setToastState({...toastState, open: true});
   };
   
   return (
@@ -30,6 +49,17 @@ const { addToCart }= useCart();
         >
         Add to Cart
       </Button>
+      <Snackbar
+        autoHideDuration={2000}
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={close}
+        key={vertical + horizontal}
+      >
+        <MuiAlert elevation={6} variant="filled" severity="success">
+          Successfully Added to Your Cart!
+        </MuiAlert>
+      </Snackbar>
     </>
   )
 }
