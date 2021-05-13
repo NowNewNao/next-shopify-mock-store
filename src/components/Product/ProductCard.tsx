c1e6c01193f821cc84586fdd54c4b964d738b544
nao <nownewnao777@gmail.com>
2021-05-13 23:26:01 +0900

    responsive

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Product } from "shopify-buy";
 
 type Props = {
@@ -7,12 +8,12 @@ type Props = {
 
 const ProductCard = ({product}: Props) => {
   return (
-    <div className="relative bg-white py-8 px-8 rounded-3xl w-96 h-112 m-4 shadow-xl hover:opacity-80">
+    <div className="relative bg-white py-4 px-4 rounded-3xl max-w-xs h-104 m-4 shadow-xl hover:opacity-80 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
       <Link href={`/products/${product.id}`}>
         <a>
           <h1 className="text-xl font-semibold mt-2 text-green-500 overflow-x-auto h-8">{product.title}</h1>
           <div className="mt-2 ">
-            <img
+            <Image
               src={product.images[0].src}
               alt={product.title}
               width={320}
@@ -20,7 +21,7 @@ const ProductCard = ({product}: Props) => {
               className="rounded-2xl"
             />
           </div>
-          <p className="overflow-y-auto h-32 text-gray-700 mt-2 px-2">
+          <p className="overflow-y-auto h-32 text-gray-700 mt-4 px-2">
             {product.description}
           </p>
         </a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,12 @@ const ProductList = ({ products }: Props) => {
   return (
     <Layout title="Home">
       <h1>Product List</h1>
-      <div className="flex items-center justify-center flex-wrap p-8 bg-gray-100">
+      <div className="flex items-center justify-center bg-gray-100">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {products.map(p => 
             <ProductCard key={p.id} product={p}/>
           )}
-
+        </div>
       </div>
     </Layout>
   )
