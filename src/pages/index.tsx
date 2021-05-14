46f03c6cfc4319d3969ad6a03cf4b4e49b252a90
nao <nownewnao777@gmail.com>
2021-05-14 09:13:42 +0900

    SET store name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ type Props = {
 const ProductList = ({ products }: Props) => {
   return (
     <Layout title="Home">
-      <h1>Product List</h1>
-      <div className="flex items-center justify-center bg-gray-100">
+      <h1 className="text-center text-3xl lg:text-4xl mt-4 lg:mt-8 font-mono">DEMO STORE</h1>
+      <div className="flex items-center justify-center bg-gray-200 mt-4 lg:mt-8">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {products.map((p) => (
             <ProductCard key={p.id} product={p} />
