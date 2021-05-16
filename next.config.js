cbbb262155b649e918b391c8bc8ad61f0a9f19ff
nao <nownewnao777@gmail.com>
2021-05-16 13:56:19 +0900

    CORRECT env

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SHOPIFY_DOMAIN: process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN,
+    NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN: process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN,
+  },
+  images: {
+    domains: ['cdn.shopify.com'],
+  },
+}
