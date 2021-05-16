f6f262e4c4f812b312e6fc69717399d345f786b0
nao <nownewnao777@gmail.com>
2021-05-16 19:34:39 +0900

    UPDATE Config type

--- src/shopify/client.tsx
+++ src/shopify/client.tsx
@@ -1,6 +1,6 @@
 import Client from 'shopify-buy';
 
 export const client = Client.buildClient({
-  domain: "mock-ec-store.myshopify.com",
-  storefrontAccessToken: `${process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN}`,
+  domain: process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN,
 });
