3a7dea6d23deeadb89e1d6ac44ae548bd29e35ca
nao <nownewnao777@gmail.com>
2021-05-16 15:04:13 +0900

    FIX build bug

--- src/shopify/client.tsx
+++ src/shopify/client.tsx
@@ -1,7 +1,6 @@
 import Client from 'shopify-buy';
 
 export const client = Client.buildClient({
-  apiVersion: "2021-04",
   domain: `${process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN}`,
   storefrontAccessToken: `${process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN}`,
 });
