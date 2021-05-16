54793a5e5550a4659fe8b6069d88da96e353e0d2
nao <nownewnao777@gmail.com>
2021-05-16 19:29:42 +0900

    SET domain

--- src/shopify/client.tsx
+++ src/shopify/client.tsx
@@ -1,6 +1,6 @@
 import Client from 'shopify-buy';
 
 export const client = Client.buildClient({
-  domain: `${process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN}`,
+  domain: "mock-ec-store.myshopify.com",
   storefrontAccessToken: `${process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN}`,
 });
